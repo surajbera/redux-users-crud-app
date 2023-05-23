7504569d0c973eec764a379d01c90e3ee899b9e0
suraj.be <suraj.be@media.net>
2023-05-23 15:50:18 +0530

    updated store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,11 @@
+/* libraries */
 import { configureStore } from '@reduxjs/toolkit'
 
+/* utilities */
+import usersReducer from 'src/features/users/usersSlice'
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    users: usersReducer,
+  },
 })
